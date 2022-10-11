61791423151ad26d68a3969e8b92599e537d57a8
Tyler Jefferson <tylerrjefferson00@gmail.com>
2022-10-11 16:02:34 -0400

    added a new object to DB

--- mongodb.js
+++ mongodb.js
@@ -7,16 +7,30 @@ const db = client.db("sample_mflix")
 const moviesCollection = db.collection("movies")
 
 //console.log( await moviesCollection.findOne({}) )
-let query = {title: { $regex: /terminator/i}} //memorize this, used to search by keywords, case insensitive
-let movieArray = await moviesCollection.find(query).limit(3).toArray()
+let query = {title: { $regex: /star wars/i}} //memorize this, used to search by keywords, case insensitive
+let movieArray = await moviesCollection
+  .find(query)
+  //.limit(3)
+  .toArray() //make it into an array
 
 for (let i = 0; i < movieArray.length; i++){
-  console.log(movieArray[i].title)
+  //console.log(movieArray[i].title)
 }
 
 //let firstMovie = movieArray[0]
 //console.log(firstMovie.title)
 
+// console.log(`There are ${movieArray.length} movies in my list`)
 
-console.log(`There are ${movieArray.length} movies in my list`)
 
+
+// add a new movie.....
+const newMovie = {
+  title: "The Boca Code Story",
+  rating: "R",
+  genre: ["Comedy"],
+  releaseDate: "2022/12/16",
+}
+
+const results = await moviesCollection.insertOne(newMovie)
+console.log("Results of insert", results)
